78c4ebb32fee89ae6af00777d61312993777d24b
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-07-24 14:53:49 -0500

    final

--- database/seeders/NewUserSeeder.php
+++ database/seeders/NewUserSeeder.php
@@ -14,11 +14,11 @@ class NewUserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate([
-            'email' => 'salafabulosa@gmail.com'
+            'email' => 'admin@xlerator.pe'
         ], [
             'name' => 'Admin',
-            'lastname' => 'SalaFabulosa',
-            'password' => '5@l4F@bul054'
+            'lastname' => 'Xlerator',
+            'password' => 'xlerator2025#'
         ])->assignRole('Admin');
     }
 }
