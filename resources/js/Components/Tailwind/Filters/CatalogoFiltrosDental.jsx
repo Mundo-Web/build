29aec84eedd2b63c7aa94cf60fcaaa542a8e6bae
brad-basilio <brad.basilioppy@gmail.com>
2025-09-24 17:51:41 -0500

    catalogo implment jireh

--- resources/js/Components/Tailwind/Filters/CatalogoFiltrosDental.jsx
+++ resources/js/Components/Tailwind/Filters/CatalogoFiltrosDental.jsx
@@ -2605,7 +2605,7 @@ const CatalogoFiltrosDental = ({ items, data, filteredData, cart, setCart, setFa
                                     {Array.isArray(products) && products.length > 0 ? (
                                         products.map((product, index) => (
                                             <motion.div
-                                                className="w-1/2 px-1 lg:w-1/3 xl:w-1/3 lg:h-[460px] lg:max-h-[500px] xl:h-[450px] xl:max-h-[450px] 2xl:h-[450px] 2xl:max-h-[450px] flex items-center justify-center"
+                                                className={`w-1/2 px-1 lg:w-1/3 xl:w-1/3 lg:h-[460px] lg:max-h-[500px] xl:h-[450px] xl:max-h-[450px] 2xl:h-[450px] 2xl:max-h-[450px] flex items-center justify-center ${data?.class_product_card_container || ''}`}
                                                 key={product.id}
                                                 initial={{ opacity: 0, y: 20, scale: 0.9 }}
                                                 animate={{ opacity: 1, y: 0, scale: 1 }}
