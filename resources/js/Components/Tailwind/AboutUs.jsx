82fa982bb7766ea2712f76991d8c5519acbea49f
brad-basilio <brad.basilioppy@gmail.com>
2025-10-15 09:51:24 -0500

    about complete

--- resources/js/Components/Tailwind/AboutUs.jsx
+++ resources/js/Components/Tailwind/AboutUs.jsx
@@ -6,6 +6,7 @@ const AboutAko = React.lazy(() => import('./AboutUs/AboutAko'))
 const AboutPaani = React.lazy(() => import('./AboutUs/AboutPaani'))
 const AboutMultivet = React.lazy(() => import('./AboutUs/AboutMultivet'))
 const AboutKatya = React.lazy(() => import('./AboutUs/AboutKatya'))
+const AboutHuaillys = React.lazy(() => import('./AboutUs/AboutHuaillys'))
 const AboutUs = ({ data, which, filteredData, items }) => {
     const getAboutUs = () => {
         switch (which) {
@@ -24,6 +25,8 @@ const AboutUs = ({ data, which, filteredData, items }) => {
                 return <AboutPaani data={data} filteredData={filteredData} items={items} />
             case 'AboutKatya':
                 return <AboutKatya data={data} filteredData={filteredData} items={items} />
+            case 'AboutHuaillys':
+                return <AboutHuaillys data={data} filteredData={filteredData} items={items} />
 
             default:
                 return <div className="w-full px-[5%] replace-max-w-here p-4 mx-auto">- No Hay componente <b>{which}</b> -</div>
