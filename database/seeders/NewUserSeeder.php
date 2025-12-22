05e40342c0e9728d14b6ed651211ec9903a561a2
Manuel Gamboa <gamboapalominocarlosmanuel@gmail.com>
2025-12-22 09:46:07 -0500

    NTs

--- database/seeders/NewUserSeeder.php
+++ database/seeders/NewUserSeeder.php
@@ -14,19 +14,11 @@ class NewUserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate([
-<<<<<<< HEAD
-            'email' => 'admin@xlerator.pe'
-        ], [
-            'name' => 'Admin',
-            'lastname' => 'Xlerator',
-            'password' => 'xlerator2025#'
-=======
             'email' => 'admin@cormarsuministros.com'
         ], [
             'name' => 'Cormar',
             'lastname' => 'Suministros',
             'password' => 'CoRm@rsU!ni5tr0s',
->>>>>>> 9ca4763587dfd555aa284f9d02988041e85f1b5e
         ])->assignRole('Admin');
     }
 }
