dee0c42fa9859030cf6b85beceb94f651e3b4324
brad-basilio <brad.basilioppy@gmail.com>
2025-12-25 11:06:42 -0500

    implementacion de footer detail mejorado

---
+++ public/build/assets/Base-BdQmyS2e.js
@@ -0,0 +1,1 @@
+import"./AboutSimple-Cf8x2fCZ.js";import"./index-BOnQTV8N.js";import"./Header-_f8YXIF9.js";import"./Footer-Bte1gxzW.js";import"./TopBar-1s_VYOem.js";import"./index-yBjzXJbu.js";import"./_commonjsHelpers-D6-XlEtG.js";import"./preload-helper-BfFHrpNk.js";
